766743e33042758976d27c58d1729da1c5694690
Denis Mironov <mironovdenis@list.ru>
2025-02-14 17:45:20 +0300

    working terrafor (but not ready)

--- terraform/modules/bigquery_module/main.tf
+++ terraform/modules/bigquery_module/main.tf
@@ -1,11 +1,16 @@
-resource "google_bigquery_dataset" "example_dataset" {
+resource "google_bigquery_dataset" "bq_dataset" {
   dataset_id = var.bq_dataset
   project    = var.gcp_project
   location   = var.gcp_region
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
-resource "google_bigquery_table" "example_table" {
-  dataset_id = google_bigquery_dataset.example_dataset.dataset_id
+resource "google_bigquery_table" "bq_table" {
+  dataset_id = google_bigquery_dataset.bq_dataset.dataset_id
   table_id   = var.bq_table
   schema     = file("${path.module}/schema.json")
+
 }
