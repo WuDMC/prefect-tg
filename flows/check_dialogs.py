8413e4a2110232ed564b23a4f210d6a7d3c7f5f8
Denis Mironov <mironovdenis@list.ru>
2025-02-18 22:00:44 +0300

    looks cool

--- flows/check_dialogs.py
+++ flows/check_dialogs.py
@@ -48,34 +48,45 @@ if __name__ == "__main__":
     load_dotenv()
 
     # ✅ Read config file path from .env
-    env_file = os.getenv('PROJECT_VARS')
-    if not env_file:
+    project_env_file = os.getenv('PROJECT_VARS')
+    local_home_folder = os.getenv("VISIONZ_HOME")
+    docker_app_folder = '/app'
+    gcp_creds = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+    if not project_env_file:
         print("❌ PROJECT_VARS is not set in .env file!")
         exit(1)
 
     # ✅ Load configuration from the specified file
     try:
-        config = Config(debug=True, env_file=env_file)
+        config = Config(debug=True, env_file=project_env_file)
 
     except Exception as e:
         print(f"❌ Error loading configuration: {e}")
         exit(1)
 
+
+
     project = config.get("prefect", "project_name")
     work_pool = config.get("prefect", "work_pool_name")
-    find_msg_4parsing()
-    # find_msg_4parsing.deploy(
-    #     name=f"{project}-find_msg_4parsing",
-    #     work_pool_name=work_pool,
-    #     image=DockerImage(
-    #         name=f"{project}-image:latest",
-    #         platform="linux/amd64",
-    #         dockerfile="_Dockerfile",
-    #     ),
-    #     job_variables={
-    #         "env": {
-    #             "VISIONZ_HOME": os.getenv("VISIONZ_HOME"),
-    #         }
-    #     },
-    #     cron="0 0,6,12,18 * * *",
-    # )
+    # find_msg_4parsing()
+    find_msg_4parsing.deploy(
+        name=f"{project}-find_msg_4parsing",
+        work_pool_name=work_pool,
+        image=DockerImage(
+            name=f"{project}-image:latest",
+            platform="linux/amd64",
+            dockerfile="_Dockerfile",
+        ),
+        job_variables={
+            "env": {
+                "VISIONZ_HOME": docker_app_folder,
+                "GOOGLE_APPLICATION_CREDENTIALS": gcp_creds.replace(local_home_folder, docker_app_folder),
+                "PROJECT_VARS": project_env_file.replace(local_home_folder, docker_app_folder),
+                "GCP_REGION": os.getenv("GCP_REGION")
+            },
+            "keep_job": True,
+            "timeout": 3600
+        },
+        cron="0 0,6,12,18 * * *",
+    )
